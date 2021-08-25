5cd38bd8dd20c03296e25845f9165a5068eec71e
nurhakame <11818022+nurhakam@users.noreply.github.com>
2021-08-25 23:22:05 +0700

    Update

--- src/pages/hiragana.jsx
+++ src/pages/hiragana.jsx
@@ -93,13 +93,11 @@ export default function Index() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   let onlyOnce = false;
 
-  function shuffle(b) {
-    const a = b;
+  function shuffle(a) {
     for (let i = a.length - 1; i > 0; i -= 1) {
       const j = Math.floor(Math.random() * (i + 1));
       [a[i], a[j]] = [a[j], a[i]];
     }
-    return a;
   }
 
   function shiragana() {
