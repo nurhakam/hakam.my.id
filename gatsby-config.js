9fc7c6e7cb267396ebd76605e518cfbaf3b90388
nurhakame <11818022+nurhakam@users.noreply.github.com>
2021-08-26 14:37:11 +0700

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     "gatsby-plugin-catch-links",
     "gatsby-plugin-advanced-sitemap",
     "gatsby-plugin-offline",
+    "gatsby-plugin-netlify",
 
     // Image and static
     {
--- src/pages/hiragana.jsx
+++ src/pages/hiragana.jsx
@@ -6,8 +6,18 @@ import SEO from "../components/SEO";
 import "../base.css";
 import hiraganaData from "../utils/hiragana";
 
+const Start = styled.button`
+  display: block;
+  background-color: #8383ce;
+  color: white;
+  border-radius: 10px;
+  border: none;
+  margin: 200px auto;
+  padding: 20px 10%;
+`;
+
 const Container = styled.div`
-  display: flex;
+  display: none;
   flex-direction: column;
   max-width: 900px;
   padding: 1rem 0 2.5rem 0;
@@ -26,28 +36,34 @@ const H1 = styled.h1`
 `;
 
 const Header = styled.div`
-  background-color: #f7ce3d;
-  color: white;
-  width: 85%;
+  border: 2px solid #f7ce3d;
+  width: 60%;
   border-radius: 10px;
   margin-top: 10px;
 `;
 
 const Score = styled.p`
-  background-color: #25bd25;
-  color: white;
   border-radius: 10px;
   margin: 10px 10px 0 0;
   padding: 8px 12px;
+  border: 2px solid #25bd25;
 `;
 
 const Alert = styled.div`
-  background-color: #acacac;
+  background-color: #5b5b5b;
   color: white;
   width: 150px;
+  padding: 10px;
+  border-radius: 10px;
+  margin: 10px 10px 0 0;
+`;
+
+const Lv = styled.div`
+  width: 50px;
   padding: 8px;
   border-radius: 10px;
   margin: 10px 10px 0 0;
+  border: 2px solid #f73d7a;
 `;
 
 const Input = styled.input`
@@ -57,6 +73,7 @@ const Input = styled.input`
   min-width: 200px;
   width: 30%;
   box-sizing: border-box;
+  border-radius: 10px;
 `;
 
 const Button = styled.button`
@@ -81,6 +98,7 @@ const Li = styled.div`
   border: 2px solid #f7ce3d;
   width: 30px;
   height: 60px;
+  border-radius: 10px;
 `;
 
 const Char = styled.div`
@@ -93,8 +111,9 @@ export default function Index() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [onlyOnce, setOnce] = useState(false);
 
-  function shuffle(a) {
+  function shuffle(b) {
     const once = true;
+    const a = b;
     for (let i = a.length - 1; i > 0; i -= 1) {
       const j = Math.floor(Math.random() * (i + 1));
       [a[i], a[j]] = [a[j], a[i]];
@@ -103,16 +122,18 @@ export default function Index() {
   }
 
   function shiragana() {
-    if (currentQuestion === 0 && onlyOnce === false) {
+    if (onlyOnce === false) {
       shuffle(hiragana);
+      document.querySelector(".start").style.display = "none";
+      document.querySelector(".container").style.display = "flex";
     }
   }
-  
-  shiragana();
 
   function validateForm() {
     const x = document.querySelector(".answer").value;
     const y = hiragana[currentQuestion].char;
+    const unknown = `unknown${y}`;
+    const char = `c${y}`;
     if (x === y && currentQuestion < 45) {
       const nextQuestion = currentQuestion + 1;
       setCurrentQuestion(nextQuestion);
@@ -120,7 +141,9 @@ export default function Index() {
       document.querySelector(`.${y}`).style.border = "2px solid #25bd25";
       document.querySelector(".result").innerHTML = "Benar!";
       document.querySelector(".result").style.backgroundColor = "#25bd25";
-      document.querySelector(`.char${y}`).style.display = "block";
+      document.querySelector(`.${y}`).style.display = "block";
+      document.querySelector(`.${unknown}`).style.display = "none";
+      document.querySelector(`.${char}`).style.display = "block";
     }
     if (x !== y) {
       document.querySelector(".result").innerHTML = "Salah!";
@@ -129,12 +152,16 @@ export default function Index() {
     if (x === y && currentQuestion === 45) {
       const lastQuestion = currentQuestion;
       const last = hiragana[lastQuestion].char;
+      const lastUnknown = `unknown${last}`;
+      const lastChar = `c${last}`;
       document.querySelector(".result").style.backgroundColor = "#25bd25";
       document.querySelector(`.${last}`).style.border = "2px solid #25bd25";
       document.querySelector(".result").innerHTML = "Selesai!";
       document.querySelector(".answer").style.display = "none";
+      document.querySelector(`.${lastUnknown}`).style.display = "none";
+      document.querySelector(`.${lastChar}`).style.display = "block";
       document.querySelector(".reload").style.display = "block";
-      document.querySelector(`.char${last}`).style.display = "block";
+      document.querySelector(`.${last}`).style.display = "block";
     }
   }
 
@@ -144,6 +171,7 @@ export default function Index() {
     let i;
     const list = document.querySelectorAll(".list");
     const char = document.querySelectorAll(".char");
+    const unkn = document.querySelectorAll(".unknown");
     document.querySelector(".answer").style.display = "block";
     document.querySelector(".answer").value = "";
     document.querySelector(".reload").style.display = "none";
@@ -152,10 +180,15 @@ export default function Index() {
     for (i = 0; i < list.length; i += 1) {
       list[i].style.border = "2px solid #f7ce3d";
       char[i].style.display = "none";
+      unkn[i].style.display = "block";
     }
     if (x === y && currentQuestion === 45) {
       const resetQuestion = 0;
+      const resetOnce = false;
       setCurrentQuestion(resetQuestion);
+      setOnce(resetOnce);
+      document.querySelector(".start").style.display = "block";
+      document.querySelector(".container").style.display = "none";
     }
   }
 
@@ -163,7 +196,10 @@ export default function Index() {
     <>
       <Helmet title="Hiragana Quiz" />
       <SEO />
-      <Container>
+      <Start className="start" type="button" onClick={() => shiragana()}>
+        Start!
+      </Start>
+      <Container className="container">
         <H1>#HiraganaQuiz</H1>
         <Header>
           <h2>{hiragana[currentQuestion].unicode}</h2>
@@ -171,38 +207,28 @@ export default function Index() {
         <Row>
           <Score>{currentQuestion + 1} / 46</Score>
           <Alert className="result">Silahkan jawab!</Alert>
+          <Lv>Lv. 1</Lv>
         </Row>
         <Input
           type="text"
           className="answer"
           autoComplete="off"
+          placeholder="type here..."
           onInput={() => validateForm()}
         />
-        <Button 
-          className="reload" 
-          type="button" 
-          onClick={() => reloadCode()}
-        >
+        <Button className="reload" type="button" onClick={() => reloadCode()}>
           Muat ulang
         </Button>
         <Row>
-          {
-            hiragana.map((hira) => (
-              <Li 
-              className={`list ${hira.char}`} 
-              key={hira.char}
-              >
-                <div>
-                  {hira.unicode}
-                  <Char 
-                  className={`char${hira.char}`}
-                  >
-                    {hira.char}
-                  </Char>
-                </div>
-              </Li>
-            ))
-          }
+          {hiragana.map((hira) => (
+            <Li className={`list ${hira.char}`} key={hira.char}>
+              <div>
+                {hira.unicode}
+                <Char className={`char c${hira.char}`}>{hira.char}</Char>
+                <div className={`unknown unknown${hira.char}`}>?</div>
+              </div>
+            </Li>
+          ))}
         </Row>
       </Container>
     </>
