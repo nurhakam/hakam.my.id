f027c331ae19f0baaedba296b7edf29b8fa73e51
nurhakame <11818022+nurhakam@users.noreply.github.com>
2022-03-06 02:16:12 +0700

    missed

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,6 @@ module.exports = {
     "gatsby-plugin-styled-components",
     "gatsby-plugin-catch-links",
     "gatsby-plugin-sitemap",
-    "gatsby-plugin-netlify",
 
     // Image and static
     {
